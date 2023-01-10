cb1bfc4b53a5d94b5905bb7cd0522a94c4bc1fcb
AhmedMousa-ag <ahmedkaremag@gmail.com>
2023-01-10 12:59:30 +0200

    init

--- hpo.py
+++ hpo.py
@@ -1,71 +1,125 @@
 #TODO: Import your dependencies.
 #For instance, below are some dependencies you might need if you are using Pytorch
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torchvision
 import torchvision.models as models
-import torchvision.transforms as transforms
-
 import argparse
 
-def test(model, test_loader):
+def test(model, test_loader,criterion,device):
     '''
     TODO: Complete this function that can take a model and a 
           testing data loader and will get the test accuray/loss of the model
           Remember to include any debugging/profiling hooks that you might need
     '''
+    print("Testing Model on Whole Testing Dataset")
+    model.eval()
+    running_loss=0
+    running_corrects=0
+    
+    for inputs, labels in test_loader:
+        inputs=inputs.to(device)
+        labels=labels.to(device)
+        outputs=model(inputs)
+        loss=criterion(outputs, labels)
+        _, preds = torch.max(outputs, 1)
+        running_loss += loss.item() * inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data).item()
+
+    total_loss = running_loss / len(test_loader.dataset)
+    total_acc = running_corrects/ len(test_loader.dataset)
+    print(f"Testing Accuracy: {100*total_acc}, Testing Loss: {total_loss}")
+    
     pass
 
-def train(model, train_loader, criterion, optimizer):
+def train(model, train_loader, criterion, optimizer,device,epochs):
     '''
     TODO: Complete this function that can take a model and
           data loaders for training and will get train the model
           Remember to include any debugging/profiling hooks that you might need
     '''
-    pass
+    model.train()
+    for e in range(epochs):
+        running_loss=0
+        correct=0
+        for data, target in train_loader:
+            data=data.to(device)
+            target=target.to(device)
+            optimizer.zero_grad()
+            pred = model(data)             #No need to reshape data since CNNs take image inputs
+            loss = criterion(pred, target)
+            running_loss+=loss
+            loss.backward()
+            optimizer.step()
+            pred=pred.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+        print(f"Epoch {e}: Loss {running_loss/len(train_loader.dataset)}, \
+            Accuracy {100*(correct/len(train_loader.dataset))}%")
+    
     
 def net():
     '''
     TODO: Complete this function that initializes your model
           Remember to use a pretrained model
     '''
-    pass
+    model = models.resnet18(pretrained=True)
+
+    for param in model.parameters():
+        param.requires_grad = False   
+
+    num_features=model.fc.in_features
+    model.fc = nn.Sequential(
+                   nn.Linear(num_features, 10))
+    return model
+    
 
 def create_data_loaders(data, batch_size):
     '''
     This is an optional function that you may or may not need to implement
     depending on whether you need to use data loaders or not
     '''
-    pass
+    return torch.utils.data.DataLoader(
+        data,
+        batch_size=batch_size,
+        shuffle=True
+    )
 
 def main(args):
     '''
     TODO: Initialize a model by calling the net function
     '''
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(f"Running on Device {device}")
     model=net()
-    
+    model=model.to(device)
+
     '''
     TODO: Create your loss and optimizer
     '''
-    loss_criterion = None
-    optimizer = None
-    
+    loss_criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.fc.parameters(), lr=args.lr, momentum=args.momentum)
     '''
     TODO: Call the train function to start training your model
     Remember that you will need to set up a way to get training data from S3
     '''
-    model=train(model, train_loader, loss_criterion, optimizer)
+    data_lengh = len(args.data)
+    train_len = int(data_lengh*0.8)
+    test_len = int(data_lengh*0.2)
+    train_data,test_data = torch.utils.data.random_split(args.data, [train_len, test_len])
+    train_loader = create_data_loaders(train_data,args.batch_size) 
+    model=train(model, train_loader, loss_criterion, optimizer,device,args.epochs)
     
     '''
     TODO: Test the model to see its accuracy
     '''
-    test(model, test_loader, criterion)
+    test_loader = create_data_loaders(test_data,args.test_batch_size)
+    criterion = nn.CrossEntropyLoss()
+    test(model, test_loader, criterion, device)
     
     '''
     TODO: Save the trained model
     '''
+    path = args.save_path
     torch.save(model, path)
 
 if __name__=='__main__':
@@ -73,7 +127,45 @@ if __name__=='__main__':
     '''
     TODO: Specify all the hyperparameters you need to use to train your model.
     '''
-    
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        default=64,
+        metavar="N",
+        help="input batch size for training (default: 64)",
+    )
+    parser.add_argument(
+        "--test-batch-size",
+        type=int,
+        default=1000,
+        metavar="N",
+        help="input batch size for testing (default: 1000)",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=5,
+        metavar="N",
+        help="number of epochs to train (default: 10)",
+    )
+    parser.add_argument(
+        "--lr", type=float, default=0.01, metavar="LR", help="learning rate (default: 0.01)"
+    )
+    parser.add_argument(
+        "--momentum", type=float, default=0.5, metavar="M", help="SGD momentum (default: 0.5)"
+    )
+    parser.add_argument(
+        "--save_path", type=str, help="The path where trained model will be saved"
+    )
+
+    """
+    # Container environment
+    parser.add_argument("--hosts", type=list, default=json.loads(os.environ["SM_HOSTS"]))
+    parser.add_argument("--current-host", type=str, default=os.environ["SM_CURRENT_HOST"])
+    parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
+    parser.add_argument("--data-dir", type=str, default=os.environ["SM_CHANNEL_TRAINING"])
+    parser.add_argument("--num-gpus", type=int, default=os.environ["SM_NUM_GPUS"])
+    """
+       
     args=parser.parse_args()
-    
-    main(args)
+    main(args) # Will call train and test
